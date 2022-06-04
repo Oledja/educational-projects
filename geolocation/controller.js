fed47faa9cc76d2e3a30154534fbd162b4d76910
oleg-prylipko <oledja1987@gmail.com>
2022-06-04 22:39:23 +0300

    deleted console.log

--- geolocation/controller.js
+++ geolocation/controller.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const findGeolocationByIp = require('./service');
 
+
 app.get('/', function (req, res) {
-    console.log(req.headers['x-forwarded-for']);
     const reqIp = req.headers['x-forwarded-for'];
     const result = findGeolocationByIp(reqIp);
     res.end(result);
