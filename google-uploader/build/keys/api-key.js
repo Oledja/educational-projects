97c3b094b9c1a9f00fd2f44d5b75406b7061bbf7
oleg-prylipko <oledja1987@gmail.com>
2022-07-11 18:53:38 +0300

    deledet oAuth2 keys

--- google-uploader/build/keys/api-key.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    api_key: "BHdidZcolR3BGHcFK0cQh8IwxSPwkGNFkMnR6w2Mf9VMh5O3e0zxWOcoeKTJ"
-};
--- google-uploader/src/keys/credentials.ts
+++ google-uploader/src/keys/credentials.ts
@@ -1,5 +1,5 @@
 export default {
-    client_id: "828421154902-j7nmlgiqi0b2o7cvk0b5st0em8unddud.apps.googleusercontent.com",
-    client_secret: "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",
-    redirect_uri: "urn:ietf:wg:oauth:2.0:oob"
+    // client_id
+    // client_secret
+    // redirect_uri
 }
