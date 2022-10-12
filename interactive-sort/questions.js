e860cd8d21bbdd20455971901d31e00ef8496857
Oleg <oledja1987@gmail.com>
2022-10-12 15:59:36 +0300

    prepared for review

---
+++ interactive-sort/questions.js
@@ -0,0 +1,12 @@
+const firstQuestion = "Enter some words separated through space: ";
+const secondQuestion = `How wold you like to sort values?
+1. Sort words alphabetically.
+2. Show digities from the smallest.
+3. Show digities from the bigest.
+4. Words by quantity of letters
+5. Show only unique words
+6. Show only unique values
+                            
+Select (1 - 6 or "exit" for EXIT) and press ENTER: `;
+
+module.exports = { firstQuestion, secondQuestion };
--- interactive-sort/util.js
+++ interactive-sort/util.js
@@ -1,70 +1,59 @@
-/**
- * @param {string} question
- * @returns {Promise<string | null>}
- */
- const readline = require('readline');
+const readline = require("readline");
 
-function isWord(str) {
-    if (isNumber(str)) {
-        return false;
-    }
-    return true;
-}
+const isWord = str => {
+  if (isNumber(str)) return false;
+  return true;
+};
 
-function isNumber(str) {
-    const result = Number(str);
-    if (isNaN(result)) {
-        return false;
-    }
-    return true;
-} 
+const isNumber = str => {
+  if (isNaN(parseInt(str))) return false;
+  return true;
+};
 
-async function ask(question) {
-    return new Promise((resolve, reject) => {
-      const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-      });
-      rl.question(question, (answer) => {
-        rl.close();
-        resolve(answer)
-      });
+const ask = async question => {
+  return new Promise(resolve => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
     });
+    rl.question(question, answer => {
+      rl.close();
+      resolve(answer);
+    });
+  });
+};
+
+const sortAndFilter = (array, sortNum) => {
+  switch (sortNum) {
+    case "1":
+      return array.filter(word => isWord(word)).sort();
+    case "2":
+      return array
+        .filter(word => isNumber(word))
+        .sort((num1, num2) => num1 - num2);
+    case "3":
+      return array
+        .filter(word => isNumber(word))
+        .sort((num1, num2) => num2 - num1);
+    case "4":
+      return array
+        .filter(word => isWord(word))
+        .sort((word1, word2) => word1.length - word2.length);
+    case "5":
+      const uniqWords = new Set();
+      array
+        .filter(word => isWord(word))
+        .forEach(word => uniqWords.add(word));
+      return Array.from(uniqWords);
+    case "6":
+      const uniqValues = new Set();
+      array.forEach(word => uniqValues.add(word));
+      return Array.from(uniqValues);
+    case "exit":
+      return "exit";
+    default:
+      return "Inсorrect input";
   }
+};
 
-  function sortAndFilter(array, sortNum) {
-    switch (sortNum) {
-        case "1":
-            return array
-                .filter(word => isWord(word))
-                .sort();
-        case "2": 
-            return array
-                .filter(word => isNumber(word))
-                .sort((num1, num2) => num1 - num2);
-        case "3":
-            return array
-                .filter(word => isNumber(word))
-                .sort((num1, num2) => num2 - num1);     
-        case "4":
-            return array
-                .filter(word => isWord(word))
-                .sort((word1, word2) => word1.length - word2.length);
-        case "5":
-            let wordsSet = new Set();
-            array
-                .filter(a => isWord(a))
-                .forEach(a => wordsSet.add(a));
-            return Array.from(wordsSet);
-        case "6":
-            let allValuesSet = new Set();
-            array.forEach(word => allValuesSet.add(word));
-            return Array.from(allValuesSet);
-        case "exit":
-            return "exit";    
-        default :
-            return "Inсorrect input"    
-    }
-}
-
-module.exports = {ask, sortAndFilter}
+module.exports = { ask, sortAndFilter };
