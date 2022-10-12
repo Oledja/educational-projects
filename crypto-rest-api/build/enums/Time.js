354fd8440eae614263ea5456499a62f026734e0c
Oleg <oledja1987@gmail.com>
2022-10-12 15:35:45 +0300

    prepared for review

--- crypto-rest-api/build/enums/Time.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var Time;
-(function (Time) {
-    Time[Time["FIFTY_MINUTES"] = 900000] = "FIFTY_MINUTES";
-    Time[Time["ONE_HOUR"] = 3600000] = "ONE_HOUR";
-    Time[Time["FOUR_HOURS"] = 14400000] = "FOUR_HOURS";
-    Time[Time["DAY"] = 86400000] = "DAY";
-})(Time || (Time = {}));
-module.exports = Time;
--- crypto-rest-api/build/utill/Time.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const time = ["FIFTY_MINUTES", "ONE_HOUR", "FOUR_HOURS", "DAY"];
-exports.default = time;
