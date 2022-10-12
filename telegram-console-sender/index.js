95039cf19bf2eb487009f8be7f16a9f23348905c
Oleg <oledja1987@gmail.com>
2022-10-12 16:27:40 +0300

    prepared for review

--- telegram-console-sender/index.js
+++ telegram-console-sender/index.js
@@ -1,36 +1,35 @@
 process.env.NTBA_FIX_319 = 1;
-const { program } = require('commander');
-const TelegramBot = require('node-telegram-bot-api');
+const { program } = require("commander");
+const TelegramBot = require("node-telegram-bot-api");
+const dotenv = require("dotenv");
 
-const token = "5452967029:AAEMGGNUJaeV13nEnIbe3l35zfamvH9NGO0";
-const chatID = 432580874;
-const bot = new TelegramBot(token, {polling: true});
+dotenv.config();
+program.version("0.0.1");
 
-program
-    .version("0.0.1")
-
-program 
-    .command("message")
-    .description("Send message to Telegram Bot")
-    .argument("<string>")
-    .alias("m")
-    .action(async function(name) {
-        try {
-            await bot.sendMessage(chatID, name);
-            process.exit();
-        } catch (err) {
-            console.log(err.message);
-        }
-    })
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
 
 program
-    .command("photo")
-    .description("Send photo to Telegram Bot")
-    .argument("path")
-    .alias("p")
-    .action(async function(path) {
-        await bot.sendPhoto(chatID, path);
-        process.exit();
-    })
-    
+  .command("message")
+  .description("Send message to Telegram Bot")
+  .argument("<string>")
+  .alias("m")
+  .action(async (name) => {
+    try {
+      await bot.sendMessage(process.env.TELEGRAM_CHAT_ID, name);
+      process.exit();
+    } catch (err) {
+      console.log(err.message);
+    }
+  });
+
+program
+  .command("photo")
+  .description("Send photo to Telegram Bot")
+  .argument("path")
+  .alias("p")
+  .action(async (path) => {
+    await bot.sendPhoto(process.env.TELEGRAM_CHAT_ID, path);
+    process.exit();
+  });
+
 program.parse(process.argv);
