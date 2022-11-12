90d151380768fa051bf433e04ad06f3b508cc50a
Oleg <oledja1987@gmail.com>
2022-11-12 14:19:10 +0200

    prepared for review

--- redistribution/handler.ts
+++
@@ -1,5 +0,0 @@
-import { sender } from "./src/lambdas/sender";
-import { receiver } from "./src/lambdas/receiver";
-import { createDB } from "./src/lambdas/createDB";
-
-export { sender, receiver, createDB };
