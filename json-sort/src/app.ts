0297645976eb4ce24dad50c4329cb6e0731d6246
Oleg <oledja1987@gmail.com>
2023-03-01 20:32:40 +0200

    reviewed

--- json-sort/src/app.ts
+++ json-sort/src/app.ts
@@ -4,6 +4,6 @@ import { readFileSync } from "fs";
 
 dotenv.config();
 
-const FILE_PATH = process.env.FILE_PATH;
-const endpoints: string[] = readFileSync(FILE_PATH, "utf-8").split("\r\n");
+const filePath = process.env.FILE_PATH;
+const endpoints: string[] = readFileSync(filePath, "utf-8").split("\r\n");
 getAllResponses(endpoints);
